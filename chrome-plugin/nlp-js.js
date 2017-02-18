1ea2619efe108f6f0442b5c00c40044a874aaa1c
Eric Cramer <emcramer@stanford.edu>
2017-02-18 15:42:25 -0800

    adding extra javascript for nlp

---
+++ chrome-plugin/nlp-js.js
@@ -0,0 +1,21 @@
+// NLP with JS
+
+var curr_url = window.location.href;
+var api_url = "https://api.diffbot.com/v3/article?";
+var api_token = "27a6981b4d8208468d629e9bc14f5f46";
+var theUrl = api_url + api_token + "&url=" + curr_url;
+
+//p perform a get request on the current page url
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+}
+
+request.responseText
